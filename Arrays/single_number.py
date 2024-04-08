ee452947845e54303b174175d6496b13f64409f1
Suneel Kumar Kanjani <skkanjani@gmail.com>
2024-04-08 23:42:25 +0500

    Create single_number.py
    solved second problem

---
+++ Arrays/single_number.py
@@ -0,0 +1,13 @@
+
+## Problem: https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/549/
+
+## Solution: 
+
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        for i in range(len(nums)):
+            c = nums.count(nums[i])
+            if c==1:
+                return c
