37c5c8c4d94740ae16b42e52233e8c61127c0ff7
Suneel Kumar Kanjani <skkanjani@gmail.com>
2024-04-15 00:21:39 +0500

    third one before I call it night

---
+++ Database/big_countries.sql
@@ -0,0 +1,9 @@
+
+-- Problem: https://leetcode.com/problems/big-countries
+-- Difficulty: Easy
+-- Solution: 
+
+/* Write your T-SQL query statement below */
+SELECT name, population, area
+FROM World
+WHERE area>=3000000 OR population>=25000000
